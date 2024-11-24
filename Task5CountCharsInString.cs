1a83d2b7a0b0dd63f9fa397ffa96fcce1af2707a
ClimaCare <imetodi90@gmail.com>
2024-11-24 19:51:39 +0200

    new task 5 added

---
+++ Task5CountCharsInString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    internal class CountCharsInString // Count Chars In a String
+    {
+        public static void Execute()
+        {
+            // Read the input string
+            string input = Console.ReadLine();
+
+            // Dictionary to store character counts
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+            // Iterate through the string
+            foreach (char ch in input)
+            {
+                if (ch != ' ') // Ignore spaces
+                {
+                    if (!charCounts.ContainsKey(ch))
+                    {
+                        charCounts[ch] = 0;
+                    }
+                    charCounts[ch]++;
+                }
+            }
+
+            // Print the result in the desired format
+            foreach (var entry in charCounts)
+            {
+                Console.WriteLine($"{entry.Key} -> {entry.Value}");
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2394da16e5bb5834de5ec09fbe62cc252f4e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ccba7b741206e34083e12ec131d1434075a217")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
